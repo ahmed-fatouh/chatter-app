1bba4bffb6d28478661d2c63d8fda858d0f346f7
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2018-06-09 00:43:39 +0200

    Make different channels as objects

---
+++ js/channels.js
@@ -0,0 +1,53 @@
+var yummy = {
+    name: 'Yummy',
+    createdOn: new Date(2016, 4, 1),
+    createdBy: 'minus.plus.yummy',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var weatherChannel = {
+    name: 'WeatherChannel',
+    createdOn: new Date(2018, 6, 4),
+    createdBy: 'array.tempting.curable',
+    starred: true,
+    expiresIn: 300,
+    messageCount: 100
+};
+
+var uniChannel = {
+    name: 'UniChannel',
+    createdOn: new Date(2018, 4, 16),
+    createdBy: 'nationality.undone.outsourcing',
+    starred: false,
+    expiresIn: 200,
+    messageCount: 400
+};
+
+var oneWorld = {
+    name: 'OneWorld',
+    createdOn: new Date(2018, 5, 15),
+    createdBy: 'wednesdays.watchers.coverage',
+    starred: true,
+    expiresIn: 500,
+    messageCount: 888
+};
+
+var bestQuotes = {
+    name: 'BestQuotes',
+    createdOn: new Date(2018, 3, 7),
+    createdBy: 'nationality.undone.outsourcing',
+    starred: false,
+    expiresIn: 50,
+    messageCount: 557
+};
+
+var placesToVisit = {
+    name: 'PlacesToVisit',
+    createdOn: new Date(2018, 5, 1),
+    createdBy: 'upgrading.never.helps',
+    starred: true,
+    expiresIn: 400,
+    messageCount: 999
+};
--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 console.log('App is alive');
 
-function switchChannel(channelName){
-    console.log('Tuning into channel "' + channelName + '".');
-    document.getElementById('current-channel-name').innerHTML = channelName;
-    document.getElementById('channel-owner-location').innerHTML = 'upgrading.never.helps';
-    document.getElementById('channel-owner-location').href = 'https://map.what3words.com/upgrading.never.helps';
-    $('#current-channel-star').removeClass().addClass('far fa-star');
+var currentChannel = weatherChannel;
+var currentLocation = {
+    longitude: 32.77427,
+    latitude: 4.526062,
+    what3words: 'array.tempting.curable'
+};
+
+function switchChannel(channel){
+    console.log('Tuning into channel "' + channel.name + '".');
+    document.getElementById('current-channel-name').innerHTML = '#' + channel.name;
+    document.getElementById('channel-owner-location').innerHTML = channel.createdBy;
+    document.getElementById('channel-owner-location').href = 'https://map.what3words.com/' + channel.createdBy;
+    $('#current-channel-star').removeClass('far fas').addClass(channel.starred ? 'fas' : 'far');
     $('li').removeClass('selected');
-    $('li:contains(' + channelName + ')').addClass('selected');
+    $('li:contains(' + channel.name + ')').addClass('selected');
+    currentChannel = channel;
 }
 
 function toggleStar(){
     $('#current-channel-star').toggleClass('far fas');
+    currentChannel.starred = !currentChannel.starred;
+    $('li:contains('+currentChannel.name+') i.fa-star').toggleClass('far fas');
 }
 
 function selectTab(tabId){
